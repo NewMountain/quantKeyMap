700df30b49285805f32efe432224d2545d68aba1
NewMountain <christopher.nyberg@gmail.com>
2016-08-09 12:25:50 -0700

    Minor Tweaks

--- keymap.c
+++ keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,         KC_W,     KC_E,   KC_R,   KC_T,   KC_TRNS,
         MO(POKR),  KC_A,         KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,   KC_Z,         KC_X,     KC_C,   KC_V,   KC_B,   KC_TRNS,
-        KC_LCTL,   KC_LGUI,      KC_TRNS,  KC_LALT,MO(HAKR),
+        KC_LCTL,   KC_LGUI,      KC_TRNS,  KC_LALT,OSL(HAKR),
                                                     KC_TRNS,  KC_TRNS,
                                                               KC_HOME,
                                                KC_BSPC,KC_DELETE,KC_END,
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LALT(LCTL(KC_DEL)),     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
              KC_TRNS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,LT(MOUZ, KC_QUOT),
-             KC_TRNS,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-                                TG(EXZL),   KC_TRNS,KC_TRNS,KC_TRNS,          KC_RCTL,
+             KC_TRNS,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                              TG(EXZL),   KC_TRNS,KC_TRNS,KC_TRNS,          KC_RCTL,
              KC_TRNS,        KC_TRNS,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  #   |           |      |------+------+------+------+------+--------|
  * |    ^   |  <-  |  |>  |   (  |   )  |  .   |------|           |------|   :  |  _   |   >  |  <$> |   =  |  """   |
  * |--------+------+------+------+------+------|  //  |           |  !=  |------+------+------+------+------+--------|
- * |        |  =>  |   $  |   [  |   ]  |  `   |      |           |      |   |  |  -   |  ->  |  /   |  ::  |   @    |
+ * |    ;   |  =>  |   $  |   [  |   ]  |  `   |      |           |      |   |  |  -   |  ->  |  /   |  ::  |   @    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -175,10 +175,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 //
 [HAKR] = KEYMAP(
-       KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_PERC,    KC_EXLM,M(B_PIPE), KC_LCBR, KC_RCBR, KC_TILD, KC_HASH,
+       KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,
+       KC_PERC,    KC_EXLM,M(B_PIPE), KC_LCBR, KC_RCBR, KC_TILD,      KC_HASH,
        KC_CIRC,  M(ASSIGN),M(F_PIPE), KC_LPRN, KC_RPRN,  KC_DOT,
-       KC_TRNS, M(JS_FUNC),   KC_DLR, KC_LBRC, KC_RBRC,  KC_GRV, KC_TRNS,
+       KC_SCLN, M(JS_FUNC),   KC_DLR, KC_LBRC, KC_RBRC,  KC_GRV,M(JS_COMMENT),
        KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
@@ -186,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,       KC_TRNS,      KC_TRNS,
    M(EQUAL_TO),  KC_AMPR, KC_PPLS,    KC_LABK, KC_PAST,M(APPLICATIVE),      M(BIND),
-                 KC_COLN, KC_UNDS,    KC_RABK,  M(FMAP),      KC_PEQL,M(DOC_STRING),
+                 KC_COLN, KC_UNDS,    KC_RABK,  M(FMAP),       KC_EQL,M(DOC_STRING),
   M(NOT_EQUAL),  KC_PIPE, KC_PMNS,M(APPLY_TO), KC_PSLS,   M(HAS_TYPE),        KC_AT,
                           KC_TRNS,    KC_TRNS, KC_TRNS,       KC_TRNS,      KC_TRNS,
        KC_TRNS, KC_TRNS,
@@ -196,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 4: EXZL
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |   Del  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |  7   |  8   |  9   |      |      |           |      |      |  +   | ArrU |  *   |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -216,10 +216,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 //
 [EXZL] = KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,    KC_7,    KC_8,    KC_9, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,    KC_4,    KC_5,    KC_6,    KC_0,
-       KC_LSFT, KC_TRNS,    KC_1,    KC_2,    KC_3, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_DEL,
+       KC_TRNS, KC_TRNS,    KC_7,    KC_8,    KC_9,       KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,    KC_4,    KC_5,    KC_6,          KC_0,
+       KC_LSFT, KC_TRNS,    KC_1,    KC_2,    KC_3,M(TRIPLE_ZERO), KC_TRNS,
        KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
@@ -227,7 +227,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_PPLS,   KC_UP, KC_PAST, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PEQL, KC_TRNS,
+                 KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT,  KC_EQL, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_PMNS, KC_TRNS, KC_PSLS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
@@ -245,73 +245,74 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       switch(id) {
     case ASSIGN:
       if(record->event.pressed){
-        return MACRO( T(LABK), T(PMNS), T(SPACE),END);
+        return MACRO( D(LSHIFT), T(LABK), U(LSHIFT), T(PMNS), END);
       }
       break;
     case JS_FUNC:
         if(record->event.pressed){
-          return MACRO( T(PEQL), T(RABK), T(SPACE),END);
+          return MACRO( T(EQL), D(LSHIFT), T(RABK), U(LSHIFT), END);
         }
         break;
     case B_PIPE:
         if(record->event.pressed){
-          return MACRO( T(LABK), T(PIPE), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(LABK), T(PIPE), U(LSHIFT), END);
         }
         break;
     case F_PIPE:
         if(record->event.pressed){
-          return MACRO( T(PIPE), T(RABK), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(PIPE), T(RABK), U(LSHIFT), END);
         }
         break;
     case JS_COMMENT:
         if(record->event.pressed){
-          return MACRO( T(PSLS), T(PSLS), T(SPACE),END);
+          return MACRO( T(SLSH), T(SLSH), END);
         }
         break;
     case EQUAL_TO:
         if(record->event.pressed){
-          return MACRO( T(PEQL), T(PEQL), T(SPACE),END);
+          return MACRO( T(EQL), T(EQL), END);
         }
         break;
     case NOT_EQUAL:
         if(record->event.pressed){
-          return MACRO( T(EXLM), T(PEQL), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(EXLM), U(LSHIFT), T(EQL), END);
         }
         break;
     case APPLY_TO:
         if(record->event.pressed){
-          return MACRO( T(PMNS), T(RABK), T(SPACE),END);
+          return MACRO( T(PMNS), D(LSHIFT), T(RABK), U(LSHIFT), END);
         }
         break;
     case FMAP:
         if(record->event.pressed){
-          return MACRO( T(LABK), T(DLR), T(RABK), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(LABK), T(DLR), T(RABK), U(LSHIFT), END);
         }
         break;
     case APPLICATIVE:
         if(record->event.pressed){
-          return MACRO( T(LABK), T(ASTR), T(RABK), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(LABK), T(ASTR), T(RABK), U(LSHIFT), END);
         }
         break;
     case HAS_TYPE:
         if(record->event.pressed){
-          return MACRO( T(COLN), T(COLN), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(COLN), T(COLN), U(LSHIFT), END);
         }
         break;
     case BIND:
         if(record->event.pressed){
-          return MACRO( T(RABK), T(RABK), T(PEQL), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(RABK), T(RABK), U(LSHIFT), T(EQL), END);
         }
         break;
     case DOC_STRING:
         if(record->event.pressed){
-          return MACRO( T(DQUO), T(DQUO), T(DQUO), T(SPACE),END);
+          return MACRO( D(LSHIFT), T(QUOT), T(QUOT), T(QUOT), U(LSHIFT), END);
         }
         break;
     case TRIPLE_ZERO:
         if(record->event.pressed){
-          RETURN MACRO( T(0), T(0), T(0), T(SPACE), END)
+          return MACRO( T(0), T(0), T(0), END);
         }
+        break;
       }
     return MACRO_NONE;
 };
